1ea1686f4f5f52dca507944b05655f22bc59f125
Anuar234 <104306302+Anuar234@users.noreply.github.com>
2025-09-13 20:47:01 +0500

    changed elementById(app)

--- todo-app/frontend/src/main.jsx
+++ todo-app/frontend/src/main.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import './style.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-const container = document.getElementById('root')
-
-const root = createRoot(container)
-
-root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("app")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
