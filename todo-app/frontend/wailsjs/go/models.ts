68adda220ffd5c714a8534e09a08467f0b0b823d
Anuar234 <104306302+Anuar234@users.noreply.github.com>
2025-09-13 20:47:46 +0500

    added simple functions from backend there

---
+++ todo-app/frontend/wailsjs/go/models.ts
@@ -0,0 +1,46 @@
+export namespace main {
+	
+	export class Task {
+	    id: number;
+	    title: string;
+	    completed: boolean;
+	    // Go type: time
+	    created_at: any;
+	    due_date?: string;
+	    priority?: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Task(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.title = source["title"];
+	        this.completed = source["completed"];
+	        this.created_at = this.convertValues(source["created_at"], null);
+	        this.due_date = source["due_date"];
+	        this.priority = source["priority"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+
+}
+
