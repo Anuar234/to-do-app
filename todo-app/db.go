b8b7fb3c230db86d05604bdb9612b1cf314d4b2a
Anuar234 <104306302+Anuar234@users.noreply.github.com>
2025-09-14 13:49:56 +0500

    database created for POstgresql integrations

---
+++ todo-app/db.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func initDB() {
+	var err error
+
+	dsn := "user=postgres password=postgres dbname=todoapp sslmode=disable port=5433"
+	db, err = sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Fatal("Failed to connect to DB: ", err)
+	}
+	log.Println("Connected to PostgreSQL")
+}
